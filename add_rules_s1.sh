a56c4a3595b50bc664ac29e18b0cf17f10344d88
awolosewicz <112141084+awolosewicz@users.noreply.github.com>
2024-06-07 05:45:22 +0000

    Fix add_rules_s1.sh wrong h2 MAC

--- add_rules_s1.sh
+++ add_rules_s1.sh
@@ -2,5 +2,5 @@
 
 simple_switch_CLI << _EOF_
 table_add ipv4_host forward 192.187.1.11 => 00:00:00:00:00:01 00:00:00:00:00:11 0
-table_add ipv4_host forward 192.187.2.11 => 00:00:00:00:00:02 00:00:00:00:00:12 1
+table_add ipv4_host forward 192.187.2.11 => 00:00:00:00:00:02 00:00:00:00:00:21 1
 _EOF_
